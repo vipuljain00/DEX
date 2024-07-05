2ebf31528016faf631d2ed30f2274490ceafa35f
vipuljain00 <arjavjain52@gmail.com>
2024-07-05 19:24:04 +0530

    NavBar

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import style from './NavBar.module.css';
+import images from '../../assets/';
+import { Model, TokenList } from '../index';
 
 const NavBar = () => {
+
+  const menuItems = [
+    {
+      name: "Swap",
+      link: "/"
+    },
+    {
+      name: "Tokens",
+      link: "/"
+    },
+    {
+      name: "Pools",
+      link: "/"
+    }]
+
+  const [openModel, setOpenModel] = useState(false);
+  const [openTokenBox, setOpenTokenBox] = useState(false);
+
   return (
-    <div>
-      NavBar
+    <div className={style.NavBar}>
+      <div className={style.NavBar_Box}>
+
+
+        {/* Left-Section */}
+        <div className={style.NavBar_Box_left}>
+
+          {/* Logo-Image */}
+          <div className={style.NavBar_Box_left_img}>
+            <Image src={images.uniswap} alt="Logo" width={50} height={50}/>
+          </div>
+
+          {/* Menu Items */}
+          <div className={style.NavBar_Box_left_menu}>
+            {menuItems.map((ele, i)=>(
+              <Link key = {i+1} href = { {pathname: `${ele.name}`, query: `${ele.link}`} }>
+                <p className={style.NavBar_Box_left_menu_item}>{ele.name}</p>
+              </Link>
+            ))}
+          </div>
+          
+        </div>
+
+
+        {/* Middle-Section */}
+        <div className={style.NavBar_Box_middle}>
+          <div className={style.NavBar_Box_middle_search}>
+
+            {/* Image */}
+            <div className={style.NavBar_Box_middle_search_image}>
+              <Image src={images.search} alt='Search Tab' width={20} height={20}/>
+            </div>
+
+            {/* Input-Section */}
+            <div className={style.NavBar_Box_middle_search_tab}>
+              <input type='text' placeholder='Search Token'/>
+            </div>
+
+          </div>
+        </div>
+
+        {/* Right-Section */}
+        <div className={style.NavBar_Box_right}>
+            <div className={style.NavBar_Box_right_img}>
+              <Image src={images.ether} alt='Network' height={30} width={30}/>
+              <p>Network Name</p>
+              <button onClick={()=>{}}>Address</button>
+              ...continue
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
