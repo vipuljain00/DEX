b51c98641ec6ac6e4ec62700d32e3387e30e1ae5
vipuljain00 <arjavjain52@gmail.com>
2024-07-10 17:15:34 +0530

    harhat

---
+++ contracts/SwapToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-Later
+pragma solidity >=0.7.0 <0.9.0;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+
+contract SwapToken {
+    
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,12 @@
+// scripts/deploy.js
+async function main() {
+    
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
