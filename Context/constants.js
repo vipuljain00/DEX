88b16c8e11270aaaea62090d8ab0f91e11975bb8
vipuljain00 <arjavjain52@gmail.com>
2024-07-17 16:39:47 +0530

    appFeature

--- Context/constants.js
+++ Context/constants.js
@@ -3,6 +3,7 @@ import LifeToken from "./LifeToken.json";
 import SingleSwapToken from "./SingleSwapToken.json"; 
 import SwapMultiHop from "./SwapMultiHop.json"; 
 import IWETH from "./IWETH.json";
+import IERC20 from "./IERC20.json"
 
 //VIP Token
 export const VipTokenAddress = "0x81f4f47aa3bBd154171C877b4d70F6C9EeCAb216";
@@ -24,6 +25,8 @@ export const SwapMultiHopABI = SwapMultiHop.abi;
 export const IWETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 export const IWETHABI = IWETH.abi;
 
+//IERC20
+export const IERC20ABI = IERC20.abi;
 // VIP token deployed to 0x81f4f47aa3bBd154171C877b4d70F6C9EeCAb216
 // LIF token deployed to 0x2ce1F0e20C1f69E9d9AEA83b25F0cEB69e2AA2b5
 // Single_Swap deployed to 0xE5b6F5e695BA6E4aeD92B68c4CC8Df1160D69A81
--- Utils/appFeature.js
+++ Utils/appFeature.js
@@ -11,7 +11,8 @@ import{
     SwapMultiHopAddress,
     SwapMultiHopABI,
     IWETHAddress,
-    IWETHABI
+    IWETHABI,
+    IERC20ABI
 } from "../Context/constants"
 
 
@@ -34,7 +35,7 @@ export const connectWallet = async()=>{
     try {
         
         if(!window.ethereum) return console.log("Install MetaMask");
-        const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+        const accounts = await window.ethereum.request({method: "eth_requestAccounts"});       //Prompts User to connect to Metmask - open the Dialog Box for connecting Wallet
         const firstAccount = accounts[0];
         return firstAccount;
 
@@ -43,10 +44,15 @@ export const connectWallet = async()=>{
     }
 };
 
+
+
+
+
 //VipToken contract Fetching
-export const fetchVipTokenContract = (signerorProvider)=>{
-    new ethers.Contract(VipTokenAddress, VipTokenABI, signerorProvider);
+export const fetchVIPContract = (signerorProvider)=>{
+    return new ethers.Contract(VipTokenAddress, VipTokenABI, signerorProvider);
 } 
+
 //Connecting with VIP Token contract
 export const connectingWithVipToken = async()=>{
     try {
@@ -55,7 +61,118 @@ export const connectingWithVipToken = async()=>{
         const connection = await web3modal.connect();
         const provider = new ethers.provider.Web3Provider(connection);
         const signer = provider.getSigner();
-        const contract = fetchVipTokenContract(signer);
+        const contract = fetchVIPContract(signer);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+//LifeToken contract Fetching
+export const fetchLIFEContract = (signerorProvider)=>{
+    return new ethers.Contract(LifeTokenAddress, LifeTokenABI, signerorProvider);
+} 
+
+//Connecting with LIFE Token contract
+export const connectingWithLifeToken = async()=>{
+    try {
+        
+        const web3modal = new Web3Modal();                                              //Initializes a new instance of Web3Modal
+        const connection = await web3modal.connect();                                   //opens a modal dialog that allows the user to select and connect their wallet - upon connection, it returns a provider object
+        const provider = new ethers.provider.Web3Provider(connection);                  // Converts the connection provider into an ethers.js provider -  allows interaction with the Ethereum blockchain through the ethers.js library
+        const signer = provider.getSigner();                                            //in the ethers.js library represents an Ethereum account that can sign transactions and messages - used to send transactions with executing functions to the Ethereum network
+        const contract = fetchLIFEContract(signer);                                     //represents a deployed smart contract on the Ethereum blockchain -  provides methods to interact with the contract’s functions
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+//SingleSwap Contract Fetching
+export const fetchSingleSwapContract = (signerorProvider)=>{
+    return new ethers.Contract(SingleSwapTokenAddress, SingleSwapTokenABI, signerorProvider);
+} 
+
+//Connecting with SingleSwap contract
+export const connectingWithSingleSwap = async()=>{
+    try {
+        
+        const web3modal = new Web3Modal();                                             
+        const connection = await web3modal.connect();                                
+        const provider = new ethers.provider.Web3Provider(connection);                 
+        const signer = provider.getSigner();                                           
+        const contract = fetchSingleSwapContract(signer);                                  
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+// //SwapMultiHop Fetching
+// export const fetchMultiHopContract = (signerorProvider)=>{
+//     return new ethers.Contract(SwapMultiHopAddress, SwapMultiHopABI, signerorProvider);
+// } 
+
+// //Connecting with LIFE Token contract
+// export const connectingWithMultiHop = async()=>{
+//     try {
+        
+//         const web3modal = new Web3Modal();                                             
+//         const connection = await web3modal.connect();                                
+//         const provider = new ethers.provider.Web3Provider(connection);                 
+//         const signer = provider.getSigner();                                           
+//         const contract = fetchMultiHopContract(signer);                                  
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+
+//IWETH Token Fetching
+export const fetchIWETHContract = (signerorProvider)=>{
+    return new ethers.Contract(IWETHAddress, IWETHABI, signerorProvider);
+} 
+
+//Connecting with LIFE Token contract
+export const connectingWithIWETH = async()=>{
+    try {
+        
+        const web3modal = new Web3Modal();                                             
+        const connection = await web3modal.connect();                                
+        const provider = new ethers.provider.Web3Provider(connection);                 
+        const signer = provider.getSigner();                                           
+        const contract = fetchIWETHContract(signer);                                  
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+//DAI Token Fetching
+const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+export const fetchDAIContract = (signerorProvider)=>{
+    return new ethers.Contract(DAIAddress, IERC20ABI, signerorProvider);
+} 
+
+//Connecting with LIFE Token contract
+export const connectingWithDAI = async()=>{
+    try {
+        
+        const web3modal = new Web3Modal();                                             
+        const connection = await web3modal.connect();                                
+        const provider = new ethers.provider.Web3Provider(connection);                 
+        const signer = provider.getSigner();                                           
+        const contract = fetchDAIContract(signer);                                  
 
     } catch (error) {
         console.log(error);
