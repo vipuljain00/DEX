94cfcfeaa58c86736effd3c1571bfc5614603073
vipuljain00 <arjavjain52@gmail.com>
2024-07-27 00:07:26 +0530

    Tokens.js

--- Pages/Tokens.js
+++ Pages/Tokens.js
@@ -50,6 +50,7 @@ const Tokens = ()=>{
     ]);
 
     const[copyAllTokenList, setCopyAllTokenList] = useState(allTokenList);
+    const[search, setSearch] = useState("");
     const[searchItem, setSearchItem] = useState(search);
 
     const onHandleSearch = (value)=>{
@@ -61,7 +62,66 @@ const Tokens = ()=>{
             setAllTokenList(copyAllTokenList);
         } else{
             setAllTokenList(filteredToken);
+        }  
+    };
+
+    const onClearSearch = ()=>{
+        if(allTokenList.length && copyAllTokenList.length){
+            setAllTokenList(copyAllTokenList);
         }
-        
-    }
+    };
+
+    useEffect(()=>{
+        const timer = setTimeout(()=>setSearch(searchItem), 1000);
+        return ()=>clearTimeout(timer);
+    }, [searchItem]);
+
+    useEffect(()=>{
+        if(search){
+            onHandleSearch(search);
+        } else{
+            onClearSearch();
+        }
+    }, [search]);
+
+    return(
+        <div className={Style.Tokens}>
+            <div className={Style.Tokens_box}>
+                <h2>Top tokens on DEX</h2>
+                
+                <div className={Style.Tokens_box_header}>
+                    <div className={Style.Tokens_box_header_ethereum}>
+                        <p>
+                            <Image 
+                                src={images.etherlogo}
+                                alt="ether"
+                                width={20}
+                                height={20}
+                            />
+                        </p>
+                        <p>Ethereum</p>
+                    </div>
+                    <div className={Style.Tokens_box_header_search}>
+                        <p>
+                            <Image
+                                src={images.search}    
+                                alt="image"
+                                width={20}
+                                height={20}
+                            />
+                        </p>
+                        <input 
+                            type="text" 
+                            placeholder="Search Token" 
+                            onChange={(e)=>setSearchItem(e.target.value)} 
+                            value={searchItem} 
+                        />
+                    </div>
+                </div>
+
+                <AllTokens allTokenList={allTokenList} />
+            </div>
+        </div>
+    )
+
 }
